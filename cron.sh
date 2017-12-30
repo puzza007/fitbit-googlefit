5a8a0d00d63f2caf2be97a1e5e74dfc41c696e84
Paul Oliver <puzza007@gmail.com>
2017-12-30 15:11:22 +1300

    fix switch in cron

--- cron.sh
+++ cron.sh
@@ -5,4 +5,4 @@ cd $(dirname $0)
 export PATH=$PATH:/usr/local/bin
 export LANG=C.UTF-8
 
-pipenv run python3 app.py -g /config/google.json -f /config/fitbit.json -s /config/config.ini -d
+pipenv run python3 app.py -g /config/google.json -f /config/fitbit.json -c /config/config.ini -d
