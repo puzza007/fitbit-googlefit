817909793a2801cab2b9ed08b76d46e14f9630d2
Paul Oliver <puzza007@gmail.com>
2018-04-17 20:36:36 +1200

    pep8

--- helpers.py
+++ helpers.py
@@ -12,40 +12,40 @@ from apiclient.discovery import build
 
 
 class Helper(object):
-	"""Helper methods to hide trivial methods"""
+    """Helper methods to hide trivial methods"""
 
-	def __init__(self, fitbitCredsFile, googleCredsFile):
-		""" Intialize a helper object.
+    def __init__(self, fitbitCredsFile, googleCredsFile):
+        """ Intialize a helper object.
 
-		fitbitCredsFile -- Fitbit credentials file
-		googleCredsFile -- Google Fits credentials file
-		"""
-		self.fitbitCredsFile = fitbitCredsFile
-		self.googleCredsFile = googleCredsFile
+        fitbitCredsFile -- Fitbit credentials file
+        googleCredsFile -- Google Fits credentials file
+        """
+        self.fitbitCredsFile = fitbitCredsFile
+        self.googleCredsFile = googleCredsFile
 
-	def GetFitbitClient(self):
-		"""Returns an authenticated fitbit client object"""
-		logging.debug("Creating Fitbit client")
-		credentials = json.load(open(self.fitbitCredsFile))  
-		client = fitbit.Fitbit(**credentials)
-		logging.debug("Fitbit client created")
-		return client
+    def GetFitbitClient(self):
+        """Returns an authenticated fitbit client object"""
+        logging.debug("Creating Fitbit client")
+        credentials = json.load(open(self.fitbitCredsFile))
+        client = fitbit.Fitbit(**credentials)
+        logging.debug("Fitbit client created")
+        return client
 
-	def GetGoogleClient(self):
-		"""Returns an authenticated google fit client object"""
-		logging.debug("Creating Google client")
-		credentials = Storage(self.googleCredsFile).get()
-		http = credentials.authorize(httplib2.Http())
-		client = build('fitness', 'v1', http=http)
-		logging.debug("Google client created")
-		return client
+    def GetGoogleClient(self):
+        """Returns an authenticated google fit client object"""
+        logging.debug("Creating Google client")
+        credentials = Storage(self.googleCredsFile).get()
+        http = credentials.authorize(httplib2.Http())
+        client = build('fitness', 'v1', http=http)
+        logging.debug("Google client created")
+        return client
 
-	def UpdateFitbitCredentials(self, fitbitClient):
-		"""Persists new fitbit credentials to local storage
+    def UpdateFitbitCredentials(self, fitbitClient):
+        """Persists new fitbit credentials to local storage
 
-		fitbitClient -- fitbit client object that contains the latest credentials
-		"""
-		credentials = json.load(open(self.fitbitCredsFile)) 
-		for t in ('access_token', 'refresh_token'):
-			credentials[t] = fitbitClient.client.token[t]
-		json.dump(credentials, open(self.fitbitCredsFile, 'w'))
+        fitbitClient -- fitbit client object that contains the latest credentials
+        """
+        credentials = json.load(open(self.fitbitCredsFile))
+        for t in ('access_token', 'refresh_token'):
+            credentials[t] = fitbitClient.client.token[t]
+        json.dump(credentials, open(self.fitbitCredsFile, 'w'))
